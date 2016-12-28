0b071e2001ceb8c90c69aa8d22c19ac2194687fa
Jens Marquardt <dev@jensmarquardt.com>
2016-12-28 17:28:46 +0100

    Remove some boilerplate stuff

---
+++ src/components/Gins.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="gins">
+        Gins
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'gins',
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
