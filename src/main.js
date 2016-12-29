3d85ac900e1d4a5aacb69250dd569d9d777fb4c7
Jens Marquardt <dev@jensmarquardt.com>
2016-12-29 14:52:49 +0100

    Add routes

--- src/main.js
+++ src/main.js
@@ -2,10 +2,29 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Router from 'vue-router'
+import Gins from './components/Gins'
+import About from './components/About'
+
+Vue.use(Router)
+
+const router = new Router({
+	mode: 'history',
+	base: __dirname,
+	routes: [
+		{ path: '/', component: Gins },
+		{ path: '/about', component: About }
+	]
+});
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-})
+	router,
+	template: `
+		<div id="app">
+			<router-link class="mdl-navigation__link" to="/">Home</router-link>
+			<router-link class="mdl-navigation__link" to="/about">About</router-link>
+			<router-view></router-view>
+		</div>
+	`
+}).$mount('#app')
