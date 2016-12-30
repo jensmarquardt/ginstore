c8034aa8cf49cb86948f203b4d72703ccbe35c89
Jens Marquardt <dev@jensmarquardt.com>
2016-12-30 21:43:41 +0100

    Fetch JSON data & build up as table

--- src/components/Gins.vue
+++ src/components/Gins.vue
@@ -1,6 +1,24 @@
 <template>
     <div class="gins container">
-        <h1 class="page-header">Gins</h1>
+        <h1 class="page-header">Manage Gins</h1>
+        <table class="table table-striped" valign="center">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Country</th>
+                    <th>Alcohol</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="gin in gins">
+                    <td>{{gin.title}}</td>
+                    <td>{{gin.country}}</td>
+                    <td>{{gin.alcohol}} % Vol</td>
+                    <td><router-link class="btn btn-default" v-bind:to="'/'">View</router-link></td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -9,7 +27,19 @@
         name: 'gins',
         data() {
             return {
+                gins: []
             }
+        },
+        methods: {
+            fetchGins() {
+                this.$http.get('/static/gins.json')
+                    .then(function(response) {
+                       this.gins = response.body;
+                    });
+            }
+        },
+        created: function() {
+            this.fetchGins();
         }
     }
 </script>
